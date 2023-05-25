2bd2f02ce9e2cf2bdc3137c94c944acf20a6906d
unknown <rhw8246@rit.edu>
2023-05-24 21:04:35 -0400

    Added bg image

--- portfolio/src/hero/Hero.tsx
+++ portfolio/src/hero/Hero.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import backgroundImage from './herobg.png'; 
 
 const Hero : React.FC = () => {
+    const heroStyle = {
+        height: "100vh",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#ffffff"
+    };
+
     return (
-        <div className="Content">
+        <div style={heroStyle}>
             <h1> Welcome </h1>
         </div>
     )
 }
 
-export default Hero
+export default Hero;
