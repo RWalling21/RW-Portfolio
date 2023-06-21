6f808ae32fbb51f6db39edcd4ff067d1d9c50123
Robert Walling <rhw8246@rit.edu>
2023-06-21 11:04:10 -0400

    Fixed AboutMe layout

--- portfolio/src/aboutMe/aboutMe.tsx
+++ portfolio/src/aboutMe/aboutMe.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Grid, Box, Text } from '@chakra-ui/react';
 
-
 const AboutMe : React.FC = () => {
     return (
         <Grid 
-            templateColumns={{ base: "1fr", md: "1fr 1fr" }} 
+            templateRows={{ base: "1fr 1fr 1fr", md: "1fr 1fr" }} 
             gap={6}
             bg="lightBlue.background" 
             minH="100vh" 
             pr="12rem" pl="12rem"
         >
-            <Box maxW="33vw" mt="4rem">
+            <Box maxW="33vw" mt="4rem" gridColumn={{ base: "1 / span 2", md: "1 / span 1" }}>
                 <Text fontSize="5xl" color="lightBlue.headline" textAlign="center"> Who am I? </Text>
                 <Text fontSize="lg" color="lightBlue.paragraph"> Eiusmod ex commodo incididunt enim aliqua ullamco eiusmod anim occaecat dolore anim. Laboris nisi aliqua culpa eu eiusmod Lorem id tempor laborum labore irure occaecat labore. Ea est laborum quis dolor laboris reprehenderit esse ea officia labore et excepteur officia ex. Duis sit consectetur nisi ullamco labore. Laborum velit laboris quis esse ad consequat ipsum excepteur minim nisi officia. </Text>
             </Box>
 
-            <Box maxW = "33vw" mt="4rem">
+            <Box maxW = "33vw" mt="4rem" gridColumn={{ base: "1 / span 2", md: "2 / span 1" }}>
                 <Text fontSize="5xl" color="lightBlue.headline" textAlign="center"> Skills </Text>
-                <Text fontSize="lg" color="lightBlue.paragraph"> Eiusmod ex commodo incididunt enim aliqua ullamco eiusmod anim occaecat dolore anim. Laboris nisi aliqua culpa eu eiusmod Lorem id tempor laborum labore irure occaecat labore. Ea est laborum quis dolor laboris reprehenderit esse ea officia labore et excepteur officia ex. Duis sit consectetur nisi ullamco labore. Laborum velit laboris quis esse ad consequat ipsum excepteur minim nisi officia. </Text>
+                
             </Box>
             
-            <Grid templateColumns="1fr" width="100%" justifyItems="center">
+            <Box gridColumn="1 / span 2">
                 <Text fontSize="5xl" color="lightBlue.headline" textAlign="center"> Books that have influenced me </Text>
-            </Grid>
+            </Box>
         </Grid>
     )
 }
