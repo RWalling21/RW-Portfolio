022cbc4a2ee5c63232dd8e3f4592c38a9693da20
unknown <rhw8246@rit.edu>
2023-06-20 23:34:25 -0400

    Seperated logic into custom hook

--- portfolio/src/header/navbar.tsx
+++ portfolio/src/header/navbar.tsx
@@ -1,47 +1,19 @@
 import React from 'react';
-import { Stack, Flex, Text, Button, Image, Box, useColorModeValue } from "@chakra-ui/react";
+import { Stack, Flex, Text, Button, Image } from "@chakra-ui/react";
 import NavButton from './NavButton';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import useNavbarAnimate from './useNavbarAnimate';
+
 const MotionFlex = motion(Flex);
 
-const Navbar : React.FC = () => {      
-    const [scrollY, setScrollY] = React.useState(0);
-    const [lastScrollTop, setLastScrollTop] = React.useState(0);
-    const [isVisible, setIsVisible] = React.useState(true);
-
-    useScrollPosition(({ currPos }) => {
-        const isScrollingUp = currPos.y > lastScrollTop;
-
-        setIsVisible(isScrollingUp || currPos.y > -80); 
-        setScrollY(currPos.y);
-        setLastScrollTop(currPos.y);
-    }, [lastScrollTop]);
-
-    const isScrolled = scrollY <= 0; 
+const Navbar : React.FC = () => {   
+    const { isVisible, motionProps } = useNavbarAnimate();
     
     return (
         <AnimatePresence>
             {isVisible && (
-                <MotionFlex
-                    initial={{ y: -125 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: -125 }}
-                    transition={{ type: 'tween', duration: 0.2 }}
-                    align="center"
-                    justify="space-between"
-                    wrap="wrap"
-                    pl="4rem" pr="4rem" pt="1rem" pb="1rem"
-                    backdropFilter='auto' backdropBlur='lg'
-                    bg={isScrolled ? "navy.background" : "transparent"} 
-                    color="navy.headline"
-                    position="fixed"
-                    top="0"
-                    zIndex="1"
-                    width="full"
-                    boxShadow={scrollY < 0 ? "0px 4px 20px rgba(0,0,0,0.1)" : "none"} 
-                    opacity={isScrolled ? 0.9 : 1}
-                >
+                <MotionFlex {...motionProps}>
                     <Flex align="center">
                         <Image src="" fallbackSrc='https://via.placeholder.com/75' mr="1rem" />
                         <Text fontSize="2xl"> Robert Walling </Text>
---
+++ portfolio/src/header/useNavbarAnimate.tsx
@@ -0,0 +1,49 @@
+// Custom hook
+import React from "react"
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+
+/**
+ * useNavbarAnimate is a custom hook that returns the props for the motion component and a boolean value indicating whether the navbar is visible.
+ * 
+ * @returns {Object} An object containing the props for the motion component and a boolean value indicating whether the navbar is visible.
+ */
+const useNavbarAnimate = () => {
+    const [scrollY, setScrollY] = React.useState(0);
+    const [lastScrollTop, setLastScrollTop] = React.useState(0);
+    const [isVisible, setIsVisible] = React.useState(true);
+
+    useScrollPosition(({ currPos }) => {
+        const isScrollingUp = currPos.y > lastScrollTop;
+
+        setIsVisible(isScrollingUp || currPos.y > -80); 
+        setScrollY(currPos.y);
+        setLastScrollTop(currPos.y);
+    }, [lastScrollTop]);
+
+    const isScrolled = scrollY <= 0; 
+
+    /**
+     * motionProps is a custom object that contains the props for the motion component.
+     */
+    const motionProps = {
+        initial: { y: -125 },
+        animate: { y: 0 },
+        exit: { y: -125 },
+        transition: { type: 'tween', duration: 0.2 },
+        color: "navy.headline",
+        position: "fixed",
+        align: "center",
+        justify: "space-between",
+        wrap: "wrap",
+        pl: "4rem", pr: "4rem", pt: "1rem", pb: "1rem",
+        backdropFilter: 'auto', backdropBlur: 'lg',
+        bg: isScrolled ? "navy.background" : "transparent",
+        top: "0", zIndex: "1", width: "full",
+        boxShadow: scrollY < 0 ? "0px 4px 20px rgba(0,0,0,0.1)" : "none",
+        opacity: isScrolled ? 0.9 : 1
+    };
+
+    return { isVisible, motionProps };
+}
+
+export default useNavbarAnimate;
