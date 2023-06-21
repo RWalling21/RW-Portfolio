f83f06ddef25645d721445e93b7fa3a6618f0d65
Robert Walling <rhw8246@rit.edu>
2023-06-21 11:10:06 -0400

    Added Skill buttons

--- portfolio/src/aboutMe/aboutMe.tsx
+++ portfolio/src/aboutMe/aboutMe.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box, Text } from '@chakra-ui/react';
+import { Grid, Box, Text, Button, HStack } from '@chakra-ui/react';
 
 const AboutMe : React.FC = () => {
     return (
@@ -11,13 +11,23 @@ const AboutMe : React.FC = () => {
             pr="12rem" pl="12rem"
         >
             <Box maxW="33vw" mt="4rem" gridColumn={{ base: "1 / span 2", md: "1 / span 1" }}>
-                <Text fontSize="5xl" color="lightBlue.headline" textAlign="center"> Who am I? </Text>
+                <Text fontSize="5xl" color="lightBlue.headline" textAlign="center" mb="1rem"> Who am I? </Text>
                 <Text fontSize="lg" color="lightBlue.paragraph"> Eiusmod ex commodo incididunt enim aliqua ullamco eiusmod anim occaecat dolore anim. Laboris nisi aliqua culpa eu eiusmod Lorem id tempor laborum labore irure occaecat labore. Ea est laborum quis dolor laboris reprehenderit esse ea officia labore et excepteur officia ex. Duis sit consectetur nisi ullamco labore. Laborum velit laboris quis esse ad consequat ipsum excepteur minim nisi officia. </Text>
             </Box>
 
             <Box maxW = "33vw" mt="4rem" gridColumn={{ base: "1 / span 2", md: "2 / span 1" }}>
-                <Text fontSize="5xl" color="lightBlue.headline" textAlign="center"> Skills </Text>
-                
+                <Text fontSize="5xl" color="lightBlue.headline" textAlign="center" mb="1rem"> Skills </Text>
+                <HStack display="flex" flexWrap="wrap" justifyContent="center" spacing={6}>
+                    <Button> Java </Button>
+                    <Button> Javascript </Button>
+                    <Button> Typescript </Button>
+                    <Button> Python </Button>
+                    <Button> C++ </Button>
+                    <Button> React </Button>
+                    <Button> Node.JS </Button>
+                    <Button> MariaDB </Button>
+                    <Button> Docker </Button>
+                </HStack>
             </Box>
             
             <Box gridColumn="1 / span 2">
