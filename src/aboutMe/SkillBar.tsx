5f12a073daee2607bfd34337d8a38511d4b74207
Robert Walling <rhw8246@rit.edu>
2023-06-23 10:50:30 -0400

    Slightly refactored about me

--- src/aboutMe/SkillBar.tsx
+++ src/aboutMe/SkillBar.tsx
@@ -3,15 +3,15 @@ import { Box, Text, Flex } from '@chakra-ui/react';
 
 const SkillBar: React.FC<{ skill: string, proficiency: number }> = ({ skill, proficiency }) => {
     return (
-        <Flex w='75vh' align="center">
+        <Flex w={['100%', '40vw']} align="center">
 
             {/* Box for Skill naming */}
-            <Box w="15vh" bg="navy.button" p="0.5rem">
+            <Box minW={["30%", "10vw"]} bg="navy.button" p="0.5rem">
                 <Text color="navy.buttonText" textAlign="center" size="sm">{skill}</Text>
             </Box>
 
             {/* Full box for proficiency */}
-            <Box w="60vh" flex="1" bg='lightBlue.cardBackground' position="relative">
+            <Box w={["70%", "30vw"]} flex="1" bg='lightBlue.cardBackground' position="relative">
                 {/* It might seem weird that I add a -1, it's to keep the bar away from text */}
                 <Box w={`${proficiency - 1}%`} h="100%" p="0.5rem" bg='#F1A5B1' position="absolute"/>
                 <Text p="0.5rem" textAlign="right">{`${proficiency}%`}</Text>
@@ -20,5 +20,6 @@ const SkillBar: React.FC<{ skill: string, proficiency: number }> = ({ skill, pro
         </Flex>
     );
 };
+
 
 export default SkillBar;
