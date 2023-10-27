69375892e7c9ca8cd7e1622c5b5173435d6ec773
Robert Walling <rhw8246@rit.edu>
2023-10-27 14:51:04 -0400

    Updated color scheme

--- src/Theme.ts
+++ src/Theme.ts
@@ -2,20 +2,13 @@ import { extendTheme } from "@chakra-ui/react";
 
 const Theme = extendTheme({
   colors: {
-    navy: {
+    main: {
       background: "#1E2024",
-      headline: "#09C6D7",
+      headline: "#00b5c6",
       paragraph: "#F3F7FF",
-      button: "#09C6D7",
+      button: "#00b5c6",
       buttonText: "#1E2024",
       buttonHover: "#E67A00"
-    },
-    lightBlue: {
-      background: "#d4d8f0",
-      headline: "#232946",
-      paragraph: "#232946",
-      cardBackground: "#fffffe",
-      cardText: "#232946",
     },
   },
 });
